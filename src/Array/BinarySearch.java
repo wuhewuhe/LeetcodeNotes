2c05dc47eba4829acd38d4eff23ac13968a29a58
He Wu <he.wu@neoxam.com>
2019-09-24 18:34:25 +0200

    binary search recursion

--- src/Array/BinarySearch.java
+++ src/Array/BinarySearch.java
@@ -3,48 +3,53 @@ package Array;
 import java.util.Arrays;
 
 public class BinarySearch {
-	
-	//t(n) = n , p(n) = 1, this is must be sorted array
+
+	// t(n) = n , p(n) = 1, this is must be sorted array
 	public static int BinarySearchNormal(int[] nums, int first, int last, int target) {
-		int mid = (first + last)/2;
-		while(first<last) {
-			if(nums[mid]<target)
+		int mid = (first + last) / 2;
+		while (first < last) {
+			if (nums[mid] < target)
 				first = mid + 1;
-			else if(nums[mid] == target)
+			else if (nums[mid] == target)
 				return mid;
-			else 
-				last = mid -1;
-			mid = (first+last)/2;
+			else
+				last = mid - 1;
+			mid = (first + last) / 2;
 		}
 		throw new IllegalArgumentException("No solution");
 	}
-	
+
 	public static int BinarySearchRecursion(int[] nums, int first, int last, int target) {
-		return target;
+		if (first > last)
+			return -1;
+		int mid = (first + last) / 2;
+		if(nums[mid]<target) return BinarySearchRecursion(nums,mid+1,last,target);
+		else if(nums[mid]<target)return BinarySearchRecursion(nums,first,mid-1,target);
+		else return mid;
 	}
 
 	public static void main(String[] args) {
-		 byte byteArr[] = {10,20,20,15,22,35}; 
-	        char charArr[] = {'g','p','q','c','i'}; 
-	        int intArr[] = {10,20,15,22,35}; 
-	        double doubleArr[] = {10.2,15.1,2.2,3.5}; 
-	        float floatArr[] = {10.2f,15.1f,2.2f,3.5f}; 
-	        short shortArr[] = {10,20,15,22,35}; 
-	  
-	        Arrays.sort(byteArr); 
-	        Arrays.sort(charArr); 
-	        Arrays.sort(intArr); 
-	        Arrays.sort(doubleArr); 
-	        Arrays.sort(floatArr); 
-	        Arrays.sort(shortArr); 
-	  
+		byte byteArr[] = { 10, 20, 20, 15, 22, 35 };
+		char charArr[] = { 'g', 'p', 'q', 'c', 'i' };
+		int intArr[] = { 10, 20, 15, 22, 35 };
+		double doubleArr[] = { 10.2, 15.1, 2.2, 3.5 };
+		float floatArr[] = { 10.2f, 15.1f, 2.2f, 3.5f };
+		short shortArr[] = { 10, 20, 15, 22, 35 };
+
+		Arrays.sort(byteArr);
+		Arrays.sort(charArr);
+		Arrays.sort(intArr);
+		Arrays.sort(doubleArr);
+		Arrays.sort(floatArr);
+		Arrays.sort(shortArr);
+
 //	        byte byteKey = 20; 
 //	        char charKey = 'g'; 
 //	        int intKey = 22; 
 //	        double doubleKey = 1.5; 
 //	        float floatKey = 35; 
 //	        short shortKey = 5; 
-	  
+
 //	        System.out.println(byteKey + " found at index = "
 //	                           +Arrays.binarySearch(byteArr,byteKey)); 
 //	        System.out.println(charKey + " found at index = "
@@ -57,8 +62,8 @@ public class BinarySearch {
 //	                           +Arrays.binarySearch(floatArr,floatKey)); 
 //	        System.out.println(shortKey + " found at index = "
 //	                           +Arrays.binarySearch(shortArr,shortKey)); 
-	        
-	        int[] nums = {10,15,20,22,35}; 
-	        System.out.println(BinarySearchNormal(nums,0,nums.length-1,20));
+
+		int[] nums = { 10, 15, 20, 22, 35 };
+		System.out.println(BinarySearchRecursion(nums, 0, nums.length - 1, 20));
 	}
 }
