a1b2633f648b211fb28e7474c3479dc5cad02dc7
He Wu <he.wu@neoxam.com>
2019-09-27 18:17:23 +0200

    house robber 337

--- src/Array/BestTimeToBuySellStockCooldown309.java
+++ src/Array/BestTimeToBuySellStockCooldown309.java
@@ -23,7 +23,7 @@ public class BestTimeToBuySellStockCooldown309 {
 
 	// brute force o(n^3)
 
-	// rest,sell,buy
+	// array optmise space
 	public int MaxProfit(int[] prices) {
 		if (prices == null || prices.length == 0 || prices.length == 1)
 			return 0;
@@ -31,12 +31,28 @@ public class BestTimeToBuySellStockCooldown309 {
 			return prices[1] > prices[0] ? prices[1] - prices[0] : 0;
 		int sell = 0, rest = 0, buy = Integer.MIN_VALUE;
 		for (int i : prices) {
-			int prev_buy = buy;
-	        buy = Math.max(sell - i, prev_buy);
-	        rest = sell;
-	        sell = Math.max(prev_buy + i, rest);
+			int prev_sell = sell;
+			sell = buy + i;
+			buy = Math.max(buy, rest-i);
+			rest = Math.max(rest, prev_sell);
 		}
 		return Math.max(rest, sell);
+	}
+
+	// dp
+	public int MaxProfit2(int[] prices) {
+		if (prices == null || prices.length == 0 || prices.length == 1)
+			return 0;
+		int[] rest = new int[prices.length], sell = new int[prices.length], hold = new int[prices.length];
+		rest[0] = 0;
+		sell[0] = 0;
+		hold[0] = -prices[0];
+		for (int i = 1; i < hold.length; i++) {
+			hold[i] = Math.max(hold[i - 1], rest[i - 1] - prices[i]);
+			sell[i] = hold[i - 1] + prices[i];
+			rest[i] = Math.max(rest[i - 1], sell[i - 1]);
+		}
+		return Math.max(rest[rest.length - 1], sell[sell.length - 1]);
 	}
 
 	@Test
