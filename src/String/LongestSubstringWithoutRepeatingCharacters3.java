a1e776a46587154ae7cbcfc27a80b741797f7054
mac <mac@MacBook-Pro-de-mac.local>
2019-10-01 23:38:04 +0200

    1 october

--- src/String/LongestSubstringWithoutRepeatingCharacters3.java
+++ src/String/LongestSubstringWithoutRepeatingCharacters3.java
@@ -1,5 +1,12 @@
 package String;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.junit.Test;
+
 /**
  * @author mac Given a string, find the length of the longest substring without
  *         repeating characters.
@@ -19,6 +26,53 @@ package String;
 public class LongestSubstringWithoutRepeatingCharacters3 {
 
 	public int lengthOfLongestSubstring(String s) {
-		
+		int n = s.length();
+		Set<Character> set = new HashSet<Character>();
+		int ans = 0, i = 0, j = 0;
+		while (i < n && j < n) {
+			// try to extend the range [i, j]
+			if (!set.contains(s.charAt(j))) {
+				set.add(s.charAt(j));
+				j++;
+				ans = Math.max(ans, j - i);
+			} else {
+				set.remove(s.charAt(i));
+				i++;
+			}
+		}
+		return ans;
+	}
+
+	// ascii time o(n)
+	public int lengthOfLongestSubstring2(String s) {
+		int n = s.length(), ans = 0;
+		int[] index = new int[128]; // current index of character
+		// try to extend the range [i, j]
+		for (int j = 0, i = 0; j < n; j++) {
+			i = Math.max(index[s.charAt(j)], i);
+			ans = Math.max(ans, j - i + 1);
+			index[s.charAt(j)] = j + 1;
+		}
+		return ans;
+	}
+
+	public int lengthOfLongestSubstring3(String s) {
+		int n = s.length(), ans = 0;
+		Map<Character, Integer> map = new HashMap<>(); // current index of character
+		// try to extend the range [i, j]
+		for (int j = 0, i = 0; j < n; j++) {
+			if (map.containsKey(s.charAt(j))) {
+				i = Math.max(map.get(s.charAt(j)), i);
+			}
+			ans = Math.max(ans, j - i + 1);
+			map.put(s.charAt(j), j + 1);
+		}
+		return ans;
+	}
+
+	@Test
+	public void test() {
+		String s = "abcabcbb";
+		System.out.println(lengthOfLongestSubstring3(s));
 	}
 }
