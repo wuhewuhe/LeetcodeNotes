983f98518d1eb43615464c9541de1f497a1138a4
wuhe <dachichiwuhe@gmail.com>
2019-10-16 11:09:26 +0200

    16 10 2019

--- src/Array/SlidingWindowSubstring76.java
+++ src/Array/SlidingWindowSubstring76.java
@@ -1,7 +1,5 @@
 package Array;
 
-import java.util.Arrays;
-
 /**
  * @author mac Given a string S and a string T, find the minimum window in S
  *         which will contain all the characters in T in complexity O(n).
@@ -47,8 +45,34 @@ public class SlidingWindowSubstring76 {
 
 	}
 
+	public static String minWindow2(String s, String t) {
+		if (t.length() == 0 || s.length() == 0 || t.length() > s.length())
+			return "";
+		int[] ans = new int[128];
+		int left = 0, count = 0, len = Integer.MAX_VALUE, begin = 0;
+		for (char c : t.toCharArray())
+			ans[c]++;
+		for (int i = 0; i < s.length(); i++) {
+			ans[s.charAt(i)]--;
+			if (ans[s.charAt(i)] >= 0)
+				count++;
+			while (count == t.length()) {
+				ans[s.charAt(left)]++;
+				if (ans[s.charAt(left)] > 0) {
+					if (len > i + 1 - left) {
+						len = i + 1 - left;
+						begin = left;
+					}
+					count--;
+				}
+				left++;
+			}
+		}
+		return len == Integer.MAX_VALUE ? "" : s.substring(begin, begin + len);
+	}
+
 	public static void main(String[] args) {
 		String S = "ADOBECODEBANC", T = "ABC";
-		System.out.println(minWindow(S, T));
+		System.out.println(minWindow2(S, T));
 	}
 }
