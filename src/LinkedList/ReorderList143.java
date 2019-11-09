eb5d9cb7f20e0d838c3d0a224166d2ae0594ded1
wuhe <dachichiwuhe@gmail.com>
2019-11-09 23:32:47 +0100

    reorder list

--- src/LinkedList/ReorderList143.java
+++ src/LinkedList/ReorderList143.java
@@ -18,17 +18,50 @@ import org.junit.Test;
 public class ReorderList143 {
 
 	public void reorderList(ListNode head) {
-		if (head == null || head.next == null)
+		if (head == null) {
 			return;
+		}
 
+		// Find the middle node
+		ListNode slow = head, fast = head.next;
+		while (fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+
+		// Reverse the second half
+		ListNode head2 = reverse(slow.next);
+		slow.next = null;
+
+		// Link the two halves together
+		while (head != null && head2 != null) {
+			ListNode tmp1 = head.next;
+			ListNode tmp2 = head2.next;
+			head2.next = head.next;
+			head.next = head2;
+			head = tmp1;
+			head2 = tmp2;
+		}
+	}
+
+	private ListNode reverse(ListNode n) {
+		ListNode prev = null;
+		ListNode cur = n;
+		while (cur != null) {
+			ListNode tmp = cur.next;
+			cur.next = prev;
+			prev = cur;
+			cur = tmp;
+		}
+		return prev;
 	}
 
 	@Test
 	public void test() {
 		ListNode n1 = new ListNode(1);
-		ListNode n2 = new ListNode(4);
+		ListNode n2 = new ListNode(2);
 		ListNode n3 = new ListNode(3);
-		ListNode n4 = new ListNode(2);
+		ListNode n4 = new ListNode(4);
 		ListNode n5 = new ListNode(5);
 		ListNode n6 = new ListNode(6);
 		n1.next = n2;
