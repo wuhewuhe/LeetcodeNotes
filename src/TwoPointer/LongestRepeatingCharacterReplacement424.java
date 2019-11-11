736f4165a32ba0aeada76400bc0ba02dbfd98692
wuhe <dachichiwuhe@gmail.com>
2019-11-11 23:21:31 +0100

    longest repeat string with k operation

--- src/TwoPointer/LongestRepeatingCharacterReplacement424.java
+++ src/TwoPointer/LongestRepeatingCharacterReplacement424.java
@@ -36,7 +36,23 @@ import org.junit.Test;
 public class LongestRepeatingCharacterReplacement424 {
 
 	public int characterReplacement(String s, int k) {
-
+		if (s == null || s.length() == 0) {
+			return 0;
+		}
+		char[] sArr = s.toCharArray();
+		int[] hash = new int[26];
+		int l = 0, maxCount = 0, result = 0;
+		//two pointer right and left are like the two side of window
+		for (int r = 0; r < sArr.length; ++r) {
+			hash[sArr[r] - 'A']++;
+			maxCount = Math.max(maxCount, hash[sArr[r] - 'A']);
+			while (r - l + 1 - maxCount > k) {
+				hash[sArr[l] - 'A']--;
+				l++;
+			}
+			result = Math.max(r - l + 1, result);
+		}
+		return result;
 	}
 
 	@Test
