185cb0bbfcda356cddf35aac5fe15e610b802be6
wuhe <dachichiwuhe@gmail.com>
2019-11-13 16:04:39 +0100

    update

--- src/TwoPointer/BasketOfFruits.java
+++ src/TwoPointer/BasketOfFruits.java
@@ -1,7 +1,11 @@
 package TwoPointer;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.junit.Test;
 
@@ -71,6 +75,58 @@ public class BasketOfFruits {
 				}
 			}
 			res = Math.max(res, i - l + 1);
+		}
+		return res;
+	}
+
+	public int totalFruit2(int[] tree) {
+		// We'll make a list of indexes for which a block starts.
+		List<Integer> blockLefts = new ArrayList<Integer>();
+
+		// Add the left boundary of each block
+		for (int i = 0; i < tree.length; ++i)
+			if (i == 0 || tree[i - 1] != tree[i])
+				blockLefts.add(i);
+
+		// Add tree.length as a sentinel for convenience
+		blockLefts.add(tree.length);
+		int ans = 0, i = 0;
+		search: while (true) {
+			// We'll start our scan at block[i].
+			// types : the different values of tree[i] seen
+			// weight : the total number of trees represented
+			// by blocks under consideration
+			Set<Integer> types = new HashSet<Integer>();
+			int weight = 0;
+			// For each block from the i-th and going forward,
+			for (int j = i; j < blockLefts.size() - 1; ++j) {
+				// Add each block to consideration
+				types.add(tree[blockLefts.get(j)]);
+				weight += blockLefts.get(j + 1) - blockLefts.get(j);
+
+				// If we have 3+ types, this is an illegal subarray
+				if (types.size() >= 3) {
+					i = j - 1;
+					continue search;
+				}
+				// If it is a legal subarray, record the answer
+				ans = Math.max(ans, weight);
+			}
+			break;
+		}
+		return ans;
+	}
+
+	public int totalFruit3(int[] tree) {
+		int res = 0, cur = 0, count_b = 0, a = 0, b = 0;
+		for (int c : tree) {
+			cur = c == a || c == b ? cur + 1 : count_b + 1;
+			count_b = c == b ? count_b + 1 : 1;
+			if (b != c) {
+				a = b;
+				b = c;
+			}
+			res = Math.max(res, cur);
 		}
 		return res;
 	}
