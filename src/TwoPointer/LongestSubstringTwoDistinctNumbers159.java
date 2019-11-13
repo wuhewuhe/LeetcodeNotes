89c0a7ffbb436ee2208430668fd03e7c6a8e8e3f
wuhe <dachichiwuhe@gmail.com>
2019-11-13 14:35:21 +0100

    hashmap and hashtable

--- src/TwoPointer/LongestSubstringTwoDistinctNumbers159.java
+++ src/TwoPointer/LongestSubstringTwoDistinctNumbers159.java
@@ -1,13 +1,19 @@
 package TwoPointer;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
 /**
  * @author mac Given a string, find the length of the longest substring T that
  *         contains at most 2 distinct characters. For example, Given s =
- *         “eceba”, T is "ece" which its length is 3.
+ *         â€œecebaâ€�, T is "ece" which its length is 3.
  * 
  */
 public class LongestSubstringTwoDistinctNumbers159 {
 
+	// hashtable
 	public int lengthOfLongestSubstringTwoDistinct(String s) {
 		if (s.length() == 0 || s == null)
 			return 0;
@@ -32,5 +38,38 @@ public class LongestSubstringTwoDistinctNumbers159 {
 			res = Math.max(res, r - l + 1);
 		}
 		return res;
+	}
+
+	// HashMap
+	public int lengthOfLongestSubstringTwoDistinct2(String s) {
+		if (s.length() == 0 || s == null)
+			return 0;
+		int l = 0, count = 0, res = 0;
+		char[] c = s.toCharArray();
+		Map<Character, Integer> map = new HashMap<Character, Integer>();
+		for (int i = 0; i < c.length; i++) {
+			if (map.containsKey(c[i])) {
+				map.put(c[i], map.get(c[i]) + 1);
+			} else {
+				map.put(c[i], 1);
+				count++;
+			}
+
+			while (count > 2) {
+				char tem = c[l++];
+				map.put(tem, map.get(tem) - 1);
+				if (map.get(tem) == 0)
+					count--;
+			}
+
+			res = Math.max(i - l + 1, res);
+		}
+		return res;
+	}
+
+	@Test
+	public void test() {
+		String s = "eceba";
+		System.out.println(lengthOfLongestSubstringTwoDistinct2(s));
 	}
 }
--- src/TwoPointer/LongestSubstringWithMostKDistinctChars340.java
+++ src/TwoPointer/LongestSubstringWithMostKDistinctChars340.java
@@ -33,10 +33,36 @@ public class LongestSubstringWithMostKDistinctChars340 {
 		return res;
 	}
 
+	// little improve
+	public String LongestSubstringKDistinct(String s, int k) {
+		if (s == null || s.length() == 0)
+			return "";
+		if (k >= s.length())
+			return s;
+		int[] hash = new int[26];
+		int lMax = 0, rMax = 0;
+		int l = 0, count = 0, res = 0;
+		for (int i = 0; i < s.length(); i++) {
+			hash[s.charAt(i) - 'a']++;
+			if (hash[s.charAt(i) - 'a'] == 1)
+				count++;
+			while (count > k) {
+				if (--hash[s.charAt(l++) - 'a'] == 0)
+					count--;
+			}
+			if (i - l + 1 > res) {
+				lMax = l;
+				rMax = i;
+				res = i - l + 1;
+			}
+		}
+		return s.substring(lMax, rMax + 1);
+	}
+
 	@Test
 	public void test() {
 		String s = "eceba";
 		int k = 2;
-		System.out.println(lengthOfLongestSubstringKDistinct(s, k));
+		System.out.println(LongestSubstringKDistinct(s, k));
 	}
 }
