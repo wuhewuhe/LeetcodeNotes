be3875413807ecdb261a2a8ef030fba4fcc4fc3c
wuhe <dachichiwuhe@gmail.com>
2019-11-14 17:18:25 +0100

    consecutive ones

---
+++ src/Array/MaxConsecutiveOnes485.java
@@ -0,0 +1,26 @@
+package Array;
+
+/**
+ * @author he.wu Given a binary array, find the maximum number of consecutive 1s
+ *         in this array.
+ * 
+ *         Example 1: Input: [1,1,0,1,1,1] Output: 3 Explanation: The first two
+ *         digits or the last three digits are consecutive 1s. The maximum
+ *         number of consecutive 1s is 3. Note:
+ * 
+ *         The input array will only contain 0 and 1. The length of input array
+ *         is a positive integer and will not exceed 10,000
+ */
+public class MaxConsecutiveOnes485 {
+	public int findMaxConsecutiveOnes(int[] nums) {
+		if (nums.length == 0 || nums == null)
+			return 0;
+		int res = 0, l = 0;
+		for (int i : nums) {
+			l = i == 0 ? 0 : l + 1;
+			res = Math.max(res, l);
+		}
+		return res;
+
+	}
+}
